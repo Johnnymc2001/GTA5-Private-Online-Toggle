291f60e176873cd656ff1031f19463abb0687052
Johnnymc2001 <37579854+Johnnymc2001@users.noreply.github.com>
2022-02-16 17:39:51 +0700

    Change UI

--- GTA5-Private-Session/Form1.Designer.cs
+++ GTA5-Private-Session/Form1.Designer.cs
@@ -29,37 +29,50 @@
         private void InitializeComponent()
         {
             this.btnToggle = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnToggle
             // 
             this.btnToggle.BackColor = System.Drawing.Color.Salmon;
-            this.btnToggle.Location = new System.Drawing.Point(12, 12);
+            this.btnToggle.Location = new System.Drawing.Point(12, 30);
             this.btnToggle.Name = "btnToggle";
-            this.btnToggle.Size = new System.Drawing.Size(116, 44);
+            this.btnToggle.Size = new System.Drawing.Size(232, 26);
             this.btnToggle.TabIndex = 3;
             this.btnToggle.Text = "Off";
             this.btnToggle.UseVisualStyleBackColor = false;
             this.btnToggle.Click += new System.EventHandler(this.btnToggle_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(198, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Toggle To Play GTA5 Online Privately";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(146, 61);
+            this.ClientSize = new System.Drawing.Size(256, 61);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnToggle);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.MdiChildrenMinimizedAnchorBottom = false;
-            this.MinimizeBox = false;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "GTA5 Private Session";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
         private Button btnToggle;
+        private Label label1;
     }
 }
